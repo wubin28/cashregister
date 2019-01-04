304d1626f723a2683188f97e736a90b15924462a
Ben WU <wubin28@gmail.com>
2019-01-04 15:25:16 +0800

    init by start.spring.io

---
+++ src/main/java/kata/testing/cashregister/CashregisterApplication.java
@@ -0,0 +1,14 @@
+package kata.testing.cashregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashregisterApplication.class, args);
+	}
+
+}
+
